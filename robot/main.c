f8563885c024722a9b9c0de694b2aec6fa600c5c
superpingu <arnaudbonetti@me.com>
2017-01-15 02:14:03 +0100

    added driver for comparators and DAC

--- robot/main.c
+++ robot/main.c
@@ -2,11 +2,13 @@
 #include "hal.h"
 #include "RTT/SEGGER_RTT.h"
 #include "exticonf.h"
+#include "compdriver.h"
 
 int main(void) {
 	halInit();
 	chSysInit();
 	initExti();
+	initComparators();
 	SEGGER_RTT_ConfigUpBuffer(0, NULL, NULL, 0, SEGGER_RTT_MODE_NO_BLOCK_TRIM);
 
 	printf("RTT !\n");
