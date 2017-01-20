f70c427aa57d3c6c851da0933e9ab795d6cc66ab
alban <albandecrevoisier@gmail.com>
2017-01-20 15:28:55 +0100

    Change spaces to hard tabs

--- tests/moving/coordination.c
+++ tests/moving/coordination.c
@@ -24,34 +24,34 @@ static double alpha;
 static int i;
 
 void update_main_coordinates(){
-    // Theta is the angle between Y axis and the orientation of the robot
-    double theta = M_PI/2 - orientation;
-    // Initializing i factor to 1, used in update_sub_coordinates
-    i = 1;
-    double xb_p;
-    double yb_p;    
+	// Theta is the angle between Y axis and the orientation of the robot
+	double theta = M_PI/2 - orientation;
+	// Initializing i factor to 1, used in update_sub_coordinates
+	i = 1;
+	double xb_p;
+	double yb_p;
 
-    /*
-     * Calculating coordinates of the next position to go to in the referential
-     * defined by the robot itself
-     */
-    xb_p = (xb - x_pos)*cos(theta) - (yb-y_pos)*sin(theta);
-    yb_p = (xb - x_pos)*sin(theta) + (yb-y_pos)*cos(theta);
-    // Are we going forward? To the left?
-    forward = SIGN(yb_p);
-    to_the_left = SIGN(xb_p);
+	/*
+	 * Calculating coordinates of the next position to go to in the referential
+	 * defined by the robot itself
+	 */
+	xb_p = (xb - x_pos)*cos(theta) - (yb-y_pos)*sin(theta);
+	yb_p = (xb - x_pos)*sin(theta) + (yb-y_pos)*cos(theta);
+	// Are we going forward? To the left?
+	forward = SIGN(yb_p);
+	to_the_left = SIGN(xb_p);
 
-    // Calculating radius of the trajectory
-    radius = (fabs(xb_p) + yb_p*yb_p/fabs(xb_p))/2;
-    // Calculating angle of the trajectory
-    alpha = 2*asin(sqrt((xb_p*xb_p)+(yb_p*yb_p))/(2*radius));
+	// Calculating radius of the trajectory
+	radius = (fabs(xb_p) + yb_p*yb_p/fabs(xb_p))/2;
+	// Calculating angle of the trajectory
+	alpha = 2*asin(sqrt((xb_p*xb_p)+(yb_p*yb_p))/(2*radius));
 }
 
 void update_sub_coordinates(){
-    // Updating distance and angle goals
-    dist_goal = forward*fabs(alpha)*radius*i/N_POINTS;
-    angle_goal = L_MM*dist_goal/(radius*U_MM)*to_the_left;
+	// Updating distance and angle goals
+	dist_goal = forward*fabs(alpha)*radius*i/N_POINTS;
+	angle_goal = L_MM*dist_goal/(radius*U_MM)*to_the_left;
 
-    // Preparing next call of the function
-    i++;
+	// Preparing next call of the function
+	i++;
 }
