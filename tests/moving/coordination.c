f0e495abfa9b72d8507ccf7dd1fbd910d42c3d16
alban <albandecrevoisier@gmail.com>
2017-01-24 16:45:18 +0100

    Coding style unification

--- tests/moving/coordination.c
+++ tests/moving/coordination.c
@@ -9,36 +9,36 @@
 #include "hal.h"
 #include "ch.h"
 
-/* return 1 if x >= 0, -1 otherwise */
+// return 1 if x >= 0, -1 otherwise
 #define SIGN(x) ((fabs(x)==x) ? 1 : -1)
 
-volatile int dist_goal = 0; /* PID->dist to next update_sub_coordinates call */
-volatile int angle_goal = 0; /* PID->tick diff: 0 is straigt, 246 is Pi/2 */
-volatile double orientation = 0; /* orientation of the robot in rad */
-volatile int x_pos = 0; /* last measured position, used when calling update_ */
-volatile int y_pos = 0; /* main_coordinates */
-volatile int last_angle_error = 0; /* computed in position.c */
-volatile int last_dist_error = 0; /* computed in position.c */
+volatile int dist_goal = 0; // PID->dist to next update_sub_coordinates call
+volatile int angle_goal = 0; // PID->tick diff: 0 is straigt, 246 is Pi/2
+volatile double orientation = 0; // orientation of the robot in rad
+volatile int x_pos = 0; // last measured position, used when calling update_
+volatile int y_pos = 0; // main_coordinates
+volatile int last_angle_error = 0; // computed in position.c
+volatile int last_dist_error = 0; // computed in position.c
 
-static double r_dep_, r_goal_; /* radius of the departure and goal circles */
-static double x_goal_, y_goal_, goal_angle_; /* variables in the new system */
-static double pt_tan_dep[2], pt_tan_goal[2]; /* tangent points */
-static int t = 0; /* 1 if inner tangent, -1 otherwise */
-static int state = 0; /* 0 departure circles, 1 straight line, 2 goal circle */
-static int i; /* i-th point out of N_POINTS in the trajectory */
-static int to_the_left; /* 1: going to the left, -1 to the right */
+static double r_dep_, r_goal_; // radius of the departure and goal circles
+static double x_goal_, y_goal_, goal_angle_; // variables in the new system
+static double pt_tan_dep[2], pt_tan_goal[2]; // tangent points
+static int t = 0; // 1 if inner tangent, -1 otherwise
+static int state = 0; // 0 departure circles, 1 straight line, 2 goal circle
+static int i; // i-th point out of N_POINTS in the trajectory
+static int to_the_left; // 1: going to the left, -1 to the right
 
-/* Called once to set the goalination */
+// Called once to set the goalination
 void update_main_coordinates(int x_goal, int y_goal, double goal_angle,
 	int r_dep, int r_goal)
 {
-	double theta = M_PI/2 - orientation; /* system rotation angle */
+	double theta = M_PI/2 - orientation; // system rotation angle
 	int dep_circle[2] = {0};
 	int goal_circle[2] = {0};
-	int tmp1[2], tmp2[2]; /* the 2 arrival circles we have to chose from */
-	double h[2] = {0}; /* homothetic center */
+	int tmp1[2], tmp2[2]; // the 2 arrival circles we have to chose from
+	double h[2] = {0}; // homothetic center
 
-	/* testing order */
+	// testing order
 	x_goal = 200;
 	y_goal = 200;
 	goal_angle = M_PI / 4;
@@ -48,13 +48,14 @@ void update_main_coordinates(int x_goal, int y_goal, double goal_angle,
 	/* Because of the method of the homothetic center, both circles cannot
 	 * be of the same radius, this dirty hack solves this issue.
 	 */
-	if (r_dep == r_goal)
+	if (r_dep == r_goal) {
 		r_dep++;
+	}
 
 	r_dep_ = r_dep;
 	r_goal_ = r_goal;
 
-	/* cartesian system change */
+	// cartesian system change
 	x_goal_ = (x_goal-x_pos)*cos(theta) - (y_goal-y_pos)*sin(theta);
 	y_goal_ = (x_goal-x_pos)*sin(theta) + (y_goal-y_pos)*cos(theta);
 	goal_angle_ = goal_angle + theta;
@@ -81,10 +82,11 @@ void update_main_coordinates(int x_goal, int y_goal, double goal_angle,
 	 * the goal is in the right-hand quadrant
 	 * hypothesis2: r_dep > r_goal TODO: all cases
 	 */
-	if (goal_circle[0] == tmp1[0])
+	if (goal_circle[0] == tmp1[0]) {
 		t = -1;
-	else
+	} else {
 		t = 1;
+	}
 
 	h[0] = (r_dep*dep_circle[0] + t*r_goal*goal_circle[0])/
 		(r_dep + t*r_goal);
@@ -133,9 +135,9 @@ void update_main_coordinates(int x_goal, int y_goal, double goal_angle,
  * Called every 50ms
  */
 void update_sub_coordinates(void) {
-	double x = 0, y = 0; /* coordinate of the destination */
-	double radius = 0; /* radius of the trajectory */
-	double alpha = 0; /* angle of the trajectory */
+	double x = 0, y = 0; // coordinate of the destination
+	double radius = 0; // radius of the trajectory
+	double alpha = 0; // angle of the trajectory
 
 	switch (state) {
 	case 0:
@@ -177,10 +179,11 @@ void update_sub_coordinates(void) {
 	dist_goal += last_dist_error;
 	angle_goal += last_angle_error;
 
-	if (i == 7)
+	if (i == 7) {
 		state = 1;
-	else if (i == 14)
+	} else if (i == 14) {
 		state = 2;
+	}
 
 	i++;
 }
--- tests/moving/coordination.h
+++ tests/moving/coordination.h
@@ -1,7 +1,7 @@
 #ifndef COORDINATION_H
 #define COORDINATION_H
 
-/* N is the number of points between to target postions the robot must go to */
+// N is the number of points between to target postions the robot must go to
 #define N_POINTS 20
 
 void update_main_coordinates(int x_goal, int y_goal, double arrival_angle,
@@ -11,5 +11,5 @@ void update_sub_coordinates(void);
 extern volatile int last_angle_error;
 extern volatile int last_dist_error;
 
-#endif /* COORDINATION_H */
+#endif // COORDINATION_H
 
