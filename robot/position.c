073eb45d11054b179152186b9160833cb1c9b6ec
Perceval Arenou <perceval.arenou@gmail.com>
2017-01-25 12:25:13 +0100

    wip : fusion x y

--- robot/position.c
+++ robot/position.c
@@ -12,6 +12,7 @@
  * Update position according to the coding wheels
  * The calculation depends on wether we are rotating to the left or to the right
  */
+
 void update_position(){
 	// Calculating current angle value in radians
 	double angle_rad = angle*U_DEGREE_ANGLE*DEG_TO_RAD;
@@ -19,8 +20,8 @@ void update_position(){
 	// Updating orientation
 	orientation += angle_rad;
 	// angle fusion
-	orientation += azimuth;
-	orientation /= 2;
+	real_orientation = orientation + azimuth;
+	real_orientation /= 2;
 
 	// Calculating last coordinates of the robot
 	x_pos += distance * cos(orientation);
@@ -35,4 +36,35 @@ void update_position(){
 	y_pos /= 2;
 }
 
+// position and orientation fusion thread
+static THD_WORKING_AREA(wa_fusion, 64);
+static THD_FUNCTION(fusion_thd, arg) {
+	(void) arg;
+	// register for radio messages datas updates
+	  void chEvtRegisterMaskWithFlags(event_source_t *esp,
+                                  event_listener_t *elp,
+                                  eventmask_t events,
+                                  eventflags_t wflags);
 
+	// 10 tuples [time, pos x, pos y]
+	int i = 0;
+	systime_t time[10];
+	float x_pos_dwm[10];
+	float y_pos_dwm[10];
+	
+	// wait for DWM message
+	chEvtBroadcastFlags(&radioEvent, EVENT_MASK(0));
+
+
+	time chVTGetSystemTime();
+	date = chThd
+	x_pos += radioData.x;
+	y_pos += radioData.y;
+
+}
+
+// To be called from main to start the enslavement with some distance and goal
+void startFusion(){
+	// Starting the monitoring threads
+	(void)chThdCreateStatic(wa_fusion, sizeof(wa_fusion), NORMALPRIO, fusion_thd, NULL);
+}
