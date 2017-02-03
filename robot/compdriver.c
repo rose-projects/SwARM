206fa6f2125b167abd0efbf3985140a3f90d2175
paulGWF <paulwguerin@gmail.com>
2017-02-03 18:41:31 +0100

    Changed hysteresis comp sources to DAC1 and vrefint/2

--- robot/compdriver.c
+++ robot/compdriver.c
@@ -9,12 +9,15 @@ volatile int up_l = 0;
 volatile int up_r = 0;
 
 void initComparators(void) {
-	RCC->APB2ENR |= 0x01; // enable SYSCFG clock (needed by comparators)
+    RCC->APB2ENR |= 0x01; // enable SYSCFG clock (needed by comparators)
+    RCC->APB1ENR |= 0x20000000; // enable DAC1 clock
+    DAC->CR = 0x01; // enable DAC 1
+    DAC->DHR8R1 = 0xC0; // by default, DAC1 outputs Vrefint*3/8
 }
 
 void up(int right){
     *UP(right) = 1;
-    *COMPCSR(right) = 0x1;
+    *COMPCSR(right) = 0x11;
     EXTI->RTSR |= MASK(right);
     EXTI->FTSR &= ~MASK(right);
     EXTI->PR = MASK(right);
@@ -22,7 +25,7 @@ void up(int right){
 
 void down(int right){
     *UP(right) = 0;
-    *COMPCSR(right) = 0x21;
+    *COMPCSR(right) = 0x41;
     EXTI->FTSR |= MASK(right);
     EXTI->RTSR &= ~MASK(right);
     EXTI->PR = MASK(right);
