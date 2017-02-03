bfe8667744b47233b953ae3a6bc02eefa8bf1a23
superpingu <arnaudbonetti@me.com>
2017-02-03 11:13:44 +0100

    PWM adapted for lock antiphase drive

--- robot/pwmdriver.h
+++ robot/pwmdriver.h
@@ -6,12 +6,12 @@ void initPWM(void);
 
 /* PWM maximum value, setting this value in the PWM channels will result in
  * a permanently high output */
-#define PWM_MAX 200
+#define PWM_MAX 400
 
-/* set left motor PWM duty cycle (from 0 to PWM_MAX) */
-#define setLpwm(val) do { TIM15->CCR2 = val; } while(0)
+/* set left motor PWM duty cycle (from -PWM_MAX/2 to PWM_MAX/2) */
+#define setLpwm(val) do { TIM15->CCR1 = val + PWM_MAX/2; TIM15->CCR2 = val + PWM_MAX/2; } while(0)
 
-/* set right motor PWM duty cycle (from 0 to PWM_MAX) */
-#define setRpwm(val) do { TIM16->CCR1 = val; } while(0)
+/* set right motor PWM duty cycle (from -PWM_MAX/2 to PWM_MAX/2) */
+#define setRpwm(val) do { TIM16->CCR1 = val + PWM_MAX/2; TIM2->CCR1 = val + PWM_MAX/2; } while(0)
 
 #endif
