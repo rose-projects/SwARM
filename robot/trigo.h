6d57ec108793e93e8ed41f100c429b2c518403ed
superpingu <arnaudbonetti@me.com>
2017-02-08 00:05:34 +0100

    improved precision of the trigo functions using linear interpolation

--- robot/trigo.h
+++ robot/trigo.h
@@ -1,8 +1,12 @@
+/* trigo.h/trigo.c define table based trigonometry functions
+ * allowing to save precious flash space while keeping a good precision */
+
 #ifndef SIN_H
 #define SIN_H
 
 float msin(float x);
 float mcos(float x);
 float macos(float x);
+float masin(float x);
 
 #endif
