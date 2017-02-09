b42bec789989c7dcdb4dc0ab53d196d62d9489dc
Perceval Arenou <perceval.arenou@gmail.com>
2017-02-09 15:10:48 +0100

    wip flash test

--- robot/position.c
+++ robot/position.c
@@ -9,7 +9,7 @@
 #include "trigo.h"
 #include "radiocomms.h"
 
-#define TRUST_DWM 0.1
+#define TRUST_DWM 0.0
 #define TRUST_IMU 0.0
 
 volatile float currentX, currentY, xSinceLastRadio, ySinceLastRadio;
